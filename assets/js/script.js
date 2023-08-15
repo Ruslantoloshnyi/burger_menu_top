f1e6ca1ddc822b608a9f60a5f41f9c90e8c176ac
Ruslan <ru@skylinehealthgroup.com>
2023-08-15 14:01:50 +0300

    finish burger top menu

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const headerBurger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+const bodyNow = document.querySelector('body');
+
+headerBurger.addEventListener('click', function () {
+    headerBurger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+    bodyNow.classList.toggle('lock');
+})
